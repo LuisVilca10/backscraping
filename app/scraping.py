38fe0f552e2f82f667b081c65eadb9ddde0a99ec
luisvilca10 <martinvilca@upeu.edu.pe>
2024-11-05 07:09:32 -0500

    Politica-Deportes

--- app/scraping.py
+++ app/scraping.py
@@ -29,7 +29,7 @@ def noticia_existe(collection_name, titulo):
     try:
         noticias_ref = db.collection(collection_name).where('titulo', '==', titulo).stream()
         for noticia in noticias_ref:
-            return True 
+            return True
         return False
     except Exception as e:
         print(f"Error al verificar la existencia de la noticia: {e}")
@@ -190,12 +190,264 @@ def scraping_andes():
     else:
         print('Error')
 
-# Programación del scraping cada 10 minutos (más razonable)
-schedule.every(0.001).minutes.do(scrape_tvsur)
-schedule.every(0.001).minutes.do(scraping_sinfronteras)
-schedule.every(0.001).minutes.do(scraping_andes)
+def scraping_sinfronterasdeportes():
+    url = 'https://diariosinfronteras.com.pe/category/deportes/'
+    response = requests.get(url)
+    noticiassf = []
 
-# Bucle para ejecutar las tareas programadas
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title_elements = soup.find_all('h3', class_='entry-title')
+
+        for title_element in title_elements:
+            title = title_element.text.strip()
+            article_url = title_element.find('a')['href']
+            article_response = requests.get(article_url)
+            if article_response.status_code == 200:
+                article_soup = BeautifulSoup(article_response.content, 'html.parser')
+                image_element = article_soup.find('img', class_='attachment-bd-normal size-bd-normal wp-post-image')
+                content_div = article_soup.find('div', class_='post-content-bd')
+                image_url = image_element['src'] if image_element else 'No Image'
+
+                if content_div:
+                    paragraphs = content_div.find_all('p')
+                    description = ''
+
+                    # Concatenar el texto de todos los párrafos
+                    for paragraph in paragraphs:
+                        description += paragraph.text.strip() + ' '
+
+                    data = {
+                        'fecha': datetime.now().strftime('%B %d, %Y'),
+                        'titulo': title,
+                        'fuente': 'Sin Fronteras',
+                        'descripcion': description.strip(),
+                        'image': image_url
+                    }
+                    noticiassf.append(data)
+
+        if noticiassf:
+            for noticia in noticiassf:
+                upload_to_firebase('deportes', noticia)
+            print("Scraping completado y datos subidos a Firebase")
+        else:
+            print("No se encontraron noticias.")
+    else:
+        print('Error al acceder a la página')
+
+
+
+
+def scraping_andes_deportes():
+    url = 'https://losandes.com.pe/category/deportes/'
+    response = requests.get(url)
+    noticias_andes = []
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title_elements = soup.find_all('h3', class_='entry-title td-module-title')
+        img_elements = soup.find_all('span', class_='entry-thumb td-thumb-css')
+        date_elements = soup.find_all('time', class_='entry-date updated td-module-date')
+
+        for title_element, img, date in zip(title_elements, img_elements, date_elements):
+            title = title_element.text.strip()
+            article_url = title_element.find('a')['href']
+            image_url = img.get('data-img-url')
+            article_response = requests.get(article_url)
+            if article_response.status_code == 200:
+                article_soup = BeautifulSoup(article_response.content, 'html.parser')
+                content_div = article_soup.find('div', class_='td_block_wrap tdb_single_content tdi_107 td-pb-border-top td_block_template_1 td-post-content tagdiv-type')
+
+                if content_div:
+                    paragraphs = content_div.find_all('p')
+                    description = ''
+
+                    # Concatenar el texto de todos los párrafos
+                    for paragraph in paragraphs:
+                        description += paragraph.text.strip() + ' '
+
+                    data = {
+                        'fecha': date.text.strip(),
+                        'titulo': title,
+                        'fuente': 'Los Andes',
+                        'descripcion': description.strip(),
+                        'image': image_url
+                    }
+                    noticias_andes.append(data)
+
+        if noticias_andes:
+            for noticia in noticias_andes:
+                upload_to_firebase('deportes', noticia)
+            print("Scraping completado y datos subidos a Firebase")
+        else:
+            print("No se encontraron noticias.")
+    else:
+        print('Error al acceder a la página')
+
+def scraping_andes_politica():
+    url = 'https://losandes.com.pe/category/politica/'
+    response = requests.get(url)
+    noticias_politica = []
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title_elements = soup.find_all('h3', class_='entry-title td-module-title')
+        img_elements = soup.find_all('span', class_='entry-thumb td-thumb-css')
+        date_elements = soup.find_all('time', class_='entry-date updated td-module-date')
+
+        for title_element, img, date in zip(title_elements, img_elements, date_elements):
+            title = title_element.text.strip()
+            article_url = title_element.find('a')['href']
+            image_url = img.get('data-img-url')
+            article_response = requests.get(article_url)
+            if article_response.status_code == 200:
+                article_soup = BeautifulSoup(article_response.content, 'html.parser')
+                content_div = article_soup.find('div', class_='td_block_wrap tdb_single_content tdi_107 td-pb-border-top td_block_template_1 td-post-content tagdiv-type')
+
+                if content_div:
+                    paragraphs = content_div.find_all('p')
+                    description = ''
+
+                    # Concatenar el texto de todos los párrafos
+                    for paragraph in paragraphs:
+                        description += paragraph.text.strip() + ' '
+
+                    data = {
+                        'fecha': date.text.strip(),
+                        'titulo': title,
+                        'fuente': 'Los Andes',
+                        'descripcion': description.strip(),
+                        'image': image_url
+                    }
+                    noticias_politica.append(data)
+
+        if noticias_politica:
+            for noticia in noticias_politica:
+                upload_to_firebase('politica', noticia)
+            print("Scraping completado y datos subidos a Firebase")
+        else:
+            print("No se encontraron noticias.")
+    else:
+        print('Error al acceder a la página')
+
+def scraping_sinfronteras_politica():
+    url = 'https://diariosinfronteras.com.pe/category/politica/'
+    response = requests.get(url)
+    noticias_politica = []
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title_elements = soup.find_all('h3', class_='entry-title')
+
+        for title_element in title_elements:
+            title = title_element.text.strip()
+            article_url = title_element.find('a')['href']
+            article_response = requests.get(article_url)
+            if article_response.status_code == 200:
+                article_soup = BeautifulSoup(article_response.content, 'html.parser')
+                image_element = article_soup.find('img', class_='attachment-bd-normal size-bd-normal wp-post-image')
+                content_div = article_soup.find('div', class_='post-content-bd')
+                image_url = image_element['src'] if image_element else 'No Image'
+
+                if content_div:
+                    paragraphs = content_div.find_all('p')
+                    description = ''
+
+                    # Concatenar el texto de todos los párrafos
+                    for paragraph in paragraphs:
+                        description += paragraph.text.strip() + ' '
+
+                    data = {
+                        'fecha': 'septiembre 4, 2024',  # Puedes ajustar esto según necesites
+                        'titulo': title,
+                        'fuente': 'Sin Fronteras',
+                        'descripcion': description.strip(),
+                        'image': image_url
+                    }
+                    noticias_politica.append(data)
+
+        if noticias_politica:
+            for noticia in noticias_politica:
+                upload_to_firebase('politica', noticia)
+            print("Scraping completado y datos subidos a Firebase")
+        else:
+            print("No se encontraron noticias.")
+    else:
+        print('Error al acceder a la página')
+        
+def scraping_tvsur_politica():
+    url = 'https://www.tvsur.com.pe/category/noticias/nacional/'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title_elements = soup.find_all('h3', class_='entry-title')
+        date_elements = soup.find_all('time', class_='entry-date updated td-module-date')
+        img_elements = soup.find_all('img', class_='entry-thumb')
+
+        noticias = []
+
+        for title_element, img, date in zip(title_elements, img_elements, date_elements):
+            article_url = title_element.find('a')['href']
+            article_response = requests.get(article_url)
+
+            if article_response.status_code == 200:
+                article_soup = BeautifulSoup(article_response.content, 'html.parser')
+                title_tag = article_soup.find('h1', class_='entry-title')
+                content_div = article_soup.find('div', class_='td-post-content')
+                title = title_tag.text.strip()
+
+                if content_div:
+                    paragraphs = content_div.find_all('p')
+                    description = ''
+
+                    # Concatenar el texto de todos los párrafos
+                    for paragraph in paragraphs:
+                        description += paragraph.text.strip() + ' '
+
+                    # Verificar y limpiar la descripción
+                    description = description.encode('utf-8', 'ignore').decode('utf-8')
+                    data_img_url = img.get('data-img-url', 'No Image')
+
+                    print(f"Title: {title}")
+                    print(f"Image: {data_img_url}")
+                    print(f"Description: {description.strip()}")
+                    print("Fecha: " + date.text.strip())
+
+                    data = {
+                        'fecha': date.text.strip(),
+                        'titulo': title,
+                        'fuente': 'TV SUR',
+                        'descripcion': description.strip(),
+                        'image': data_img_url
+                    }
+
+                    noticias.append(data)
+
+                else:
+                    print("Error al encontrar el contenido de la noticia.")
+            else:
+                print("Error al acceder al artículo.")
+
+        if noticias:
+            for noticia in noticias:
+                upload_to_firebase('politica', noticia)  # Cambiar a la colección que necesites
+            print("Scraping completado y datos subidos a Firebase.")
+        else:
+            print("No se encontraron noticias.")
+    else:
+        print('Error al acceder a la página.')
+        
+def scrape_all():
+    scrape_tvsur()
+    scraping_sinfronteras()
+    scraping_andes()
+    scraping_sinfronterasdeportes()
+    scraping_andes_deportes()
+    scraping_andes_politica()
+    scraping_sinfronteras_politica()
+schedule.every(0.0001).minutes.do(scrape_all)
+
 while True:
     schedule.run_pending()
     time.sleep(1)
